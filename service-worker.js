7f05f30304d51305bef73a75f2c26cdb9b0f3a32
jecwb <112090249+jecwb@users.noreply.github.com>
2026-02-19 08:28:11 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "41cast-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/doar.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
